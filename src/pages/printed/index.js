0bb5fb36d256aaf375c2755a29b88b3a6e285abc
Theo <theoleanse@gmail.com>
2019-12-31 15:53:12 +0000

    add all pages with custom grid (#2)

--- src/pages/printed/index.js
+++ src/pages/printed/index.js
@@ -1,38 +1,42 @@
-import { Link } from 'gatsby';
 import React from 'react';
 import Layout from '../../components/Layout';
+import { PageTemplate } from '../../components/Items';
 
-const printedItems = [
-	{
-		id: 'printed-item',
-		src:
-			'http://www.kirafreije.com/images/Kira_Freije_Forgetting_and_Unforgetting_2017_Cass_Sculpture_Foundation',
-		description: 'This is a descrition'
-	},
-	{
-		id: 'printed-item',
-		src:
-			'http://www.kirafreije.com/images/Kira_Freije_Forgetting_and_Unforgetting_2017_Cass_Sculpture_Foundation',
-		description: 'This is a descrition'
-	}
-];
-
-const PrintedItem = ({ id, src, alt, description }) => {
-	return (
-		<div>
-			<Link to={`/printed/${id}`}>
-				<img src={src} alt={alt} />
-			</Link>
-			<p>{description}</p>
-		</div>
-	);
+const pageModel = {
+	title: 'Home',
+	layout: 'flexible',
+	heading: null,
+	items: [
+		{
+			type: 'image-text-right',
+			link: '/printed/printed-item',
+			text: 'text',
+			grid: {
+				full: false,
+				columnStart: 3,
+				columnEnd: 6
+				// or: `span: 4`
+			},
+			src:
+				'http://www.kirafreije.com/images/Kira_Freije_Forgetting_and_Unforgetting_2017_Cass_Sculpture_Foundation'
+		},
+		{
+			type: 'image-text-right',
+			link: '/printed/printed-item',
+			text: 'text',
+			grid: {
+				full: false,
+				columnStart: 7,
+				columnEnd: 10
+			},
+			src:
+				'http://www.kirafreije.com/images/Kira_Freije_Forgetting_and_Unforgetting_2017_Cass_Sculpture_Foundation'
+		}
+	]
 };
-const InfoPage = () => (
+
+export default () => (
 	<Layout>
-		{printedItems.map(props => (
-			<PrintedItem {...props} />
-		))}
+		<PageTemplate {...pageModel} />
 	</Layout>
 );
-
-export default InfoPage;
