5f9c6e58e37774fbdc167985600813319fa410aa
Theo Leanse <theoleanse@gmail.com>
2020-01-01 10:51:57 +0000

    media queries

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,86 +1,45 @@
 import { Link } from 'gatsby';
 import React from 'react';
 
-const fullWidthStyle = {
-	gridColumn: '1 / 11',
-	marginBottom: '50px',
-	width: '100vw',
-	position: 'relative',
-	left: '50%',
-	right: '50%',
-	marginLeft: '-50vw',
-	marginRight: '-50vw'
+const getStyle = ({ full, columnStart, columnEnd }) => {
+	return full
+		? {}
+		: {
+				gridColumn: `${columnStart} / ${columnEnd}`
+		  };
 };
 
 const Image = ({ src, alt, link, grid, text }) => {
 	return link ? (
 		<Link
 			to={link}
-			className=""
+			className={`image ${grid.full ? 'full-width' : 'variable-width'}`}
 			title=""
-			style={
-				grid.full
-					? fullWidthStyle
-					: {
-							gridColumn: `${grid.columnStart} / ${grid.columnEnd}`, // or gridColumn: `${grid.columnStart} / span ${grid.span}`
-							marginBottom: '50px'
-					  }
-			}
+			style={getStyle(grid)}
 		>
-			<img src={src} alt={alt} />
+			{src ? <img src={src} alt={alt} /> : null}
 			{text ? <p>{text}</p> : null}
 		</Link>
 	) : (
 		<div
-			style={
-				grid.full
-					? fullWidthStyle
-					: {
-							gridColumn: `${grid.columnStart} / ${grid.columnEnd}`,
-							marginBottom: '50px'
-					  }
-			}
+			className={`image ${grid.full ? 'full-width' : 'variable-width'}`}
+			style={getStyle(grid)}
 		>
-			<img src={src} alt={alt} />
+			{src ? <img src={src} alt={alt} /> : null}
 			{text ? <p>{text}</p> : null}
-		</div>
-	);
-};
-
-const Text = ({ text, grid }) => {
-	return (
-		<div
-			style={
-				grid.full
-					? fullWidthStyle
-					: {
-							gridColumn: `${grid.columnStart} / ${grid.columnEnd}`,
-							marginBottom: '50px'
-					  }
-			}
-		>
-			<p>{text}</p>
 		</div>
 	);
 };
 
 export const PageTemplate = ({ items }) => (
-	<div
-		style={{
-			display: 'grid',
-			gridTemplateColumns: 'repeat(10, 10%)',
-			width: '95%',
-			margin: '0 auto'
-		}}
-	>
+	<div className="page">
 		{items.map(props => {
 			switch (props.type) {
 				case 'image':
 				case 'image-text-below':
 				case 'image-text-right':
-					return <Image {...props} />;
 				case 'text':
-					return <Text {...props} />;
+					return <Image {...props} />;
 				default:
 					break;
 			}
